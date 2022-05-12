0947327da1e9b71b42ee7ae6b7e6f6485f03807f
Juan Mosquella <jmosquella11@gmail.com>
2022-05-12 00:47:03 -0300

    Mejoras en el codigo

--- app.js
+++ app.js
@@ -12,17 +12,7 @@ let lifePoints = 12;
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    Swal.fire({
-        imageUrl: './img/start.jpg',
-        imageHeight: 540,
-        title: 'Bienvenido a Rick y Morty Memorys!',
-        text: 'Encuentra las coincidencias de cada personaje',  
-        width: '80%',      
-        confirmButtonText: 'Empezar',
-        allowOutsideClick:'false',
-        // grow:'fullscreen',
-        allowOutsideClick: false
-    })
+   
 
     const fetchData = async () => {
     
@@ -59,75 +49,79 @@ document.addEventListener('DOMContentLoaded', () => {
     
     let validation = []
     
-    document.addEventListener('click', (e) => {   
+    document.addEventListener('click', (e) => {  
+
+        if(e.target.matches('#reload')){
+            location.reload()
+        } 
         
         
         if(e.target.matches('.card') || e.target.matches('.card *')){           
     
             const card = e.target.parentElement.parentElement    
             card.classList.add('flip')    
-            validation.push(card)           
+            validation.push(card)
+            
+            const plusOne = () => {
+                const pointsContainer = document.getElementById('points')        
+                points++;
+                pointsContainer.textContent = points;
+                if(points === 8){
+                    
+                }    
+            }   
+            
+            const wrongCard = () => {
     
-            if(validation.length === 2){                
+                const lifeContainer = document.getElementById('lifes');
+                lifePoints--;
+                lifeContainer.textContent = lifePoints
+        
+                if(lifePoints === 0){
+                    console.log('perdiste el juego')
+                    document.querySelectorAll('.card').forEach(card => {
+                        card.style.pointerEvents='none'
+                    })
+                    
+
+                }    
+            }
+    
+            if(validation.length === 2){               
+                
                 
                 if(validation[0].dataset.id == validation[1].dataset.id){
-                    setTimeout(() => {
-                        
-                        validation.forEach(el => el.style.pointerEvents = 'none')
-                        validation = [];
-                        plusOne()                    
-                        
-                    }, 1000);
+                    validation.forEach(el => {
+                        el.style.pointerEvents = 'none';
+                        el.classList.add('valid')
+                        validation = []
+                    })
+                    
+                    plusOne()  
                 }else{
-                    setTimeout(() => {
+                    setTimeout(() => {                        
                         
                         validation.forEach(el => el.classList.remove('flip'))                   
-                        validation = []
-                        wrongCard()
                         
-                    }, 1000);                
-                }            
-            }                         
-        }  
+                        wrongCard()
+                        validation = []
+                        
+                        
+                    }, 600);                
+                } 
+
+                
+                
+                
+                
+                
+            } 
+                                
+        } 
         
-        const plusOne = () => {
-            const pointsContainer = document.getElementById('points')        
-            points++;
-            pointsContainer.textContent = points;
-            if(points === 8){
-                Swal.fire({
-                    imageUrl: './img/Win.jpg',
-                    imageHeight: 540,
-                    title: 'Wubba lubba dub dub!',
-                    text: 'Has encontrado todas las coincidencias',  
-                    width: '80%',      
-                    confirmButtonText: 'Jugar otra vez',
-                    allowOutsideClick:'false',
-                    // grow:'fullscreen',
-                    allowOutsideClick: false
-                })
-            }    
-        }
+        
+        
     
-        const wrongCard = () => {
-    
-            const lifeContainer = document.getElementById('lifes');
-            lifePoints--;
-            lifeContainer.textContent = lifePoints
-    
-            if(lifePoints === 0){
-                Swal.fire({
-                    imageUrl: './img/Lost.jpg',
-                    imageHeight: 540,
-                    title: 'Perdiste!',
-                    text: 'Intenta encontrar todas las coincidencias sin perder todas las vidas',  
-                    width: '80%',      
-                    confirmButtonText: 'Volver a intentarlo',
-                    allowOutsideClick:'false',
-                    // grow:'fullscreen',
-                    allowOutsideClick: false
-                })
-            }    
-        }
+        
     })
 })
